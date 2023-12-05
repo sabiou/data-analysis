532c6345ca6bf11b4c86892c85033c139a91a85a
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 16:16:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,17 +36,25 @@ percentage_table['Percentage'] = (pivot_table['Total'] / pivot_table['Total'].lo
 st.write("## Pivot Table")
 st.write(pivot_table.join(percentage_table['Percentage']))
 
-# Create an Altair scatter chart
-scatter_chart = alt.Chart(df).mark_circle().encode(
-    x='ANNEE:N',
-    y='QUANTITE A COMMANDER( BOITES):Q',
-    color='Profil:N',
-    tooltip=['Profil:N', 'ANNEE:N', 'QUANTITE A COMMANDER( BOITES):Q']
-).properties(
-    width=600,
-    height=400
+
+# Create a pivot table with 'DEMANDEUR' as the index
+pivot_table_demandeur = pd.pivot_table(
+    df,
+    values='QUANTITE A COMMANDER( BOITES)',
+    index=['DEMANDEUR'],
+    columns=['ANNEE'],
+    aggfunc={'QUANTITE A COMMANDER( BOITES)': 'sum'},
+    margins=True,
+    margins_name='Total'
 )
 
-# Display the Altair scatter chart using Streamlit
-st.write("## Scatter Chart")
-st.altair_chart(scatter_chart, use_container_width=True)
+# Calculate percentages for each cell
+percentage_table_demandeur = (pivot_table_demandeur.div(pivot_table_demandeur.loc[:, 'Total'], axis=0) * 100).round(2)
+
+# Add a 'Percentage' column
+percentage_table_demandeur['Percentage'] = (pivot_table_demandeur['Total'] / pivot_table_demandeur['Total'].loc['Total'] * 100).round(2)
+
+# Display the new pivot table with original values and percentage using Streamlit
+st.write("## Pivot Table with DEMANDEUR as Index")
+st.write(pivot_table_demandeur.join(percentage_table_demandeur['Percentage']))
+
