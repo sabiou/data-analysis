1299623507b66266984f8c8d48b7c2b9095d20d3
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 17:15:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import locale
+from io import StringIO
 
 
 # Set the locale to French
@@ -77,7 +78,22 @@ st.write("## Pivot Table for All Records Corresponding to 'Centrale pharmaceutiq
 st.write(pivot_table_all_records)
 
 
-df_bar_plot = pivot_table_all_records
+# Read the provided table from a string
+table_string = """DEMANDEUR	2018	2019	2020	2021	2022	Total	Percentage
+CEPROPHARM	0	0	600	6500	33163	40263	3.214
+CODIPHARM	0	7000	0	0	0	7000	0.559
+EGY NILE PHARM	0	0	0	0	3000	3000	0.239
+HUMANWELL PHARMA	0	0	0	0	83800	83800	6.69
+LABOREX	39308	41252	27873	124310	83461	316204	25.242
+ONPPC	4500	18391	16082	113082	108334	260389	20.786
+SAPHAR	7268	9094	8078	5500	6892	36832	2.94
+SONIMED SARL	0	1124	1728	750	22880	26482	2.114
+SONIPHARM	0	0	0	0	430	430	0.034
+UBIPHARM	22856	89990	47745	173537	144157	478285	38.181
+"""
+
+# Load the table into a DataFrame
+df_bar_plot = pd.read_csv(StringIO(table_string), delim_whitespace=True)
 
 # Create a bar plot using Altair
 bar_chart = alt.Chart(df_bar_plot).mark_bar().encode(
