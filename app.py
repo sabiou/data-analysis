d8f5345182b19fb6ebf6227a3fafd2fc5c0ab42d
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 16:25:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,28 +14,6 @@ df = pd.read_excel(file_path)
 st.write("## Displaying DataFrame")
 st.write(df)
 
-
-# Create a pivot table
-pivot_table = pd.pivot_table(
-    df,
-    values='QUANTITE A COMMANDER( BOITES)',
-    index=['Profil'],
-    columns=['ANNEE'],
-    aggfunc={'QUANTITE A COMMANDER( BOITES)': 'sum'},
-    margins=True,
-    margins_name='Total'
-)
-
-# Calculate percentages for each cell
-percentage_table = (pivot_table.div(pivot_table.loc[:, 'Total'], axis=0) * 100).round(2)
-
-# Add a 'Percentage' column
-percentage_table['Percentage'] = (pivot_table['Total'] / pivot_table['Total'].loc['Total'] * 100).round(2)
-
-# Display the pivot table with original values and percentage using Streamlit
-st.write("## Pivot Table")
-st.write(pivot_table.join(percentage_table['Percentage']))
-
 # Drop rows where 'DEMANDEUR' is null
 df = df.dropna(subset=['DEMANDEUR'])
 
