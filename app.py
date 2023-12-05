ffd819e329a3fa3fd9e00ee8fc4df57228056e12
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 17:01:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,11 @@ pivot_table_all_records['Total'] = pivot_table_all_records.sum(axis=1)
 # Calculate the percentage for each row
 pivot_table_all_records['Percentage'] = (pivot_table_all_records['Total'] / pivot_table_all_records['Total'].sum() * 100).round(2)
 
+# Add a 'Total General' row
+total_general_row = pd.DataFrame(pivot_table_all_records.sum()).T
+total_general_row.index = ['Total General']
+pivot_table_all_records = pd.concat([pivot_table_all_records, total_general_row])
+
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table for All Records Corresponding to 'Centrale pharmaceutique'")
 st.write(pivot_table_all_records)
