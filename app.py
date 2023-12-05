ff4b34b844e87284c1a1aa6c4a67b8a16a57d155
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 23:54:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -429,7 +429,6 @@ st.write(updated_frame)
 
 
 
-
 # Create a pivot table
 pivot_table = pd.pivot_table(
     df,
@@ -441,7 +440,7 @@ pivot_table = pd.pivot_table(
 )
 
 # Calculate percentages for each cell
-percentage_table = (pivot_table.div(pivot_table.loc[:, 'Total'], axis=0) * 100).round(2)
+percentage_table = (pivot_table.div(pivot_table['Total'], axis=0) * 100).round(2)
 
 # Add a 'Percentage' column
 percentage_table['Percentage'] = (pivot_table['Total'] / pivot_table['Total'].loc['Total'] * 100).round(2)
