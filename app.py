69626828edfeb5ff65c42deb1aa16ee5b9adfa7a
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-06 00:54:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -482,10 +482,9 @@ pivot_table_cont = pd.pivot_table(
 # Calculate percentages for each cell
 percentage_cont = (pivot_table_cont.div(pivot_table_cont.loc[:, 'Total'], axis=0) * 100).round(2)
 
+# Add a 'Percentage' column
+percentage_cont['Percentage'] = (pivot_table_cont['Total'] / pivot_table_cont['Total'].loc['Total'] * 100).round(2)
+
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table")
-st.write(pivot_table_cont)
-
-# Display the pivot table with percentages
-st.write("## Pivot Table with Percentages")
-st.write(percentage_cont)
+st.write(pivot_table_cont.join(percentage_cont['Percentage']))
