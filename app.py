b74b7b9839f7b8d1039e7534c5c4c6ec59a83aca
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 17:20:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,35 +76,3 @@ pivot_table_all_records = pd.concat([pivot_table_all_records, total_general_row]
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table for All Records Corresponding to 'Centrale pharmaceutique'")
 st.write(pivot_table_all_records)
-
-
-
-# Read the provided pivot table from a string
-pivot_table_string = """DEMANDEUR	2018	2019	2020	2021	2022	Total	Percentage
-CEPROPHARM	0	0	600	6500	33163	40263	3.214
-CODIPHARM	0	7000	0	0	0	7000	0.559
-EGY NILE PHARM	0	0	0	0	3000	3000	0.239
-HUMANWELL PHARMA	0	0	0	0	83800	83800	6.69
-LABOREX	39308	41252	27873	124310	83461	316204	25.242
-ONPPC	4500	18391	16082	113082	108334	260389	20.786
-SAPHAR	7268	9094	8078	5500	6892	36832	2.94
-SONIMED SARL	0	1124	1728	750	22880	26482	2.114
-SONIPHARM	0	0	0	0	430	430	0.034
-UBIPHARM	22856	89990	47745	173537	144157	478285	38.181
-"""
-
-# Load the pivot table into a DataFrame
-pivot_table_all_records = pd.read_csv(StringIO(pivot_table_string), delim_whitespace=True)
-
-# Create a bar plot using Altair
-bar_chart = alt.Chart(pivot_table_all_records).mark_bar().encode(
-    x=alt.X('Percentage:Q', axis=alt.Axis(title='Percentage')),
-    y=alt.Y('DEMANDEUR:N', sort='-x', axis=alt.Axis(title='DEMANDEUR')),
-    tooltip=['Percentage']
-).properties(
-    title="Percentage Distribution by DEMANDEUR"
-)
-
-# Display the bar chart using Streamlit
-st.write("## Bar Plot for Percentage Distribution")
-st.altair_chart(bar_chart, use_container_width=True)
