a1255072100fc41d2b7ddc75387d8f40706ec07e
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 15:23:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import locale
 
 
 # Set the locale to French
-locale.setlocale(locale.LC_NUMERIC, 'fr_FR.UTF-8')
+locale.setlocale(locale.LC_ALL, "fr_FR")
 
 file_path = "database.xlsx"
 df = pd.read_excel(file_path)
