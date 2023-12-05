547f055cc09d65d067df11d32a77b96876b5d864
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 15:14:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,19 @@ percentage_table['Percentage'] = (percentage_table['Total'] / percentage_table['
 # Display the pivot table using Streamlit
 st.write("## Pivot Table")
 st.write(percentage_table)
+
+# Create an Altair chart for visualization
+melted_table = pd.melt(pivot_table.reset_index(), id_vars='Profil', var_name='Year', value_name='Quantity')
+chart = alt.Chart(melted_table).mark_bar().encode(
+    x='Year:N',
+    y='Quantity:Q',
+    color='Profil:N',
+    tooltip=['Profil:N', 'Year:N', 'Quantity:Q']
+).properties(
+    width=600,
+    height=400
+)
+
+# Display the Altair chart using Streamlit
+st.write("## Altair Chart")
+st.altair_chart(chart, use_container_width=True)
