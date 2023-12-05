76c980df1510a924ba6eea97f528080e316d8f05
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-05 14:29:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
 
+
+file_path = "database.xls"
+df = pd.read_excel(file_path)
+
+
+st.write("## Displaying DataFrame")
+st.write(df)
