5066d14ba1f245b7ca3e7cdae1e805fd39d61f76
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-18 22:38:12 +0100

    update

--- app.py
+++ app.py
@@ -80,11 +80,11 @@ pivot_table_all_records['Pourcentage'] = (pivot_table_all_records['Total'] / piv
 # Add a 'Total General' row
 total_general_row = pd.DataFrame(pivot_table_all_records.sum()).T
 total_general_row.index = ['Total General']
-pivot_table_all_records = pd.concat([pivot_table_all_records, total_general_row])
+pivot_table_all_records = pd.concat([pivot_table_all_records['Pourcentage'], total_general_row])
 
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table for All Records Corresponding to 'Centrale pharmaceutique'")
-st.write(pivot_table_all_records['Pourcentage'].style.format(thousands="", precision=2, decimal=","))
+st.write(pivot_table_all_records.style.format(thousands="", precision=2, decimal=","))
 
 
 # Filter the data for the value "Centrale pharmaceutique" in the "Profil" column
