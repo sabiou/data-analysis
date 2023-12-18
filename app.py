a86cd2eb10890bda8416d06b7d78d1ce0e15f087
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-18 22:33:36 +0100

    update

--- app.py
+++ app.py
@@ -84,8 +84,7 @@ pivot_table_all_records = pd.concat([pivot_table_all_records, total_general_row]
 
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table for All Records Corresponding to 'Centrale pharmaceutique'")
-st.write(pivot_table_all_records.style.format(thousands="", precision=2, decimal=","))
-
+st.write(pivot_table_all_records['Pourcentage'].style.format(thousands="", precision=2, decimal=","))
 
 
 # Filter the data for the value "Centrale pharmaceutique" in the "Profil" column
