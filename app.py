8ca7f42a714fa518f38db769ec930bc5ccca7a07
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-18 21:51:11 +0100

    update

--- app.py
+++ app.py
@@ -46,7 +46,8 @@ percentage_table['Pourcentage'] = (pivot_table['Total'] / pivot_table['Total'].l
 
 # Display the pivot table with original values and percentage using Streamlit
 st.write("## Pivot Table")
-st.write(pivot_table.join(percentage_table['Pourcentage'].head().style.format(thousands="")))
+st.write(pivot_table.head().style.format(thousands=""))
+st.write(pivot_table.join(percentage_table['Pourcentage']))
 
 
 # Filter the data for the value "Centrale pharmaceutique" in the "Profil" column
