dba7cc8881263955aa8640645a8bfbf5e8e89902
Farouk Sabiou <sfarouk298@gmail.com>
2023-12-19 01:23:34 +0100

    update

--- app.py
+++ app.py
@@ -469,8 +469,16 @@ total_general_row_c['Pourcentage'] = 100.0  # Assuming 100% for the 'Pourcentage
 updated_frame_classe = pd.concat([pivot_table_profile_classe, total_general_row_c])
 
 # Display the updated DataFrame
+#st.write("## Updated DataFrame with 'Total General'")
+#st.write(updated_frame_classe.style.format(thousands="", precision=2, decimal=","))
+# Display the updated DataFrame with proper formatting
 st.write("## Updated DataFrame with 'Total General'")
-st.write(updated_frame_classe.style.format(thousands="", precision=2, decimal=","))
+st.write(
+    updated_frame
+    .style.format({
+        col: "{:,.2f}" if "%" not in col else "{:.6f}" for col in updated_frame.columns
+    })
+)
 
 ### 
 
